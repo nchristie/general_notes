816f268c9672b5705067975162b71cf32e293771
Naomi Christie <naomi.christie@gmail.com>
2019-12-28 23:34:06 +0000

    Work on make_links.sh

--- make_links.sh
+++ make_links.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-link_files (link_text) {
+link_files () {
+	link_text=$1
 	for d in */; do
-		sed -i -e 's/$link_text/link_text/g' $d*
+		sed -i -e "s/ $link_text/ $link_text[$link_text]/g" $d*;
 	done
 }
-
 
 for d in */; do 
 	for filename in $d*; do 
 		str1="${filename/.md/}"; 
-		str2="${str1/*\//}";
+		link_text="${str1/*\//}";
+		link_files $link_text
 	done; 
 done
 
